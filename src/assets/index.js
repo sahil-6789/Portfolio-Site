32a120699d5a70b3995929f655a06eaea8ef81ac
Sahil <sahiltyagi6789@gmail.com>
2023-08-11 10:17:51 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import academy from "./company/academy.png";
+
+import food from "./food.jpg";
+import expense from "./expense.jpg";
+import tripguide from "./tripguide.png";
+import jobshala from "./jobshala.jpg";
+import tindog from "./tindog.jpg";
+import shopping from "./shopping.png";
+import todo from "./todolist.jpg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+ academy,
+  food,
+  expense,
+  tripguide,
+  tindog,
+  shopping,
+  todo,
+  jobshala
+};
---
+++ src/constants/index.js
@@ -0,0 +1,282 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  food,
+  expense,
+  jobshala,
+  tripguide,
+  threejs,
+  academy,
+  todo,
+  shopping,
+  tindog
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "C++ Programer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML ",
+    icon: html,
+  },
+  {
+    name: "CSS ",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+   {
+    title: "DSA Teaching Assistant",
+    company_name: "10X Academy",
+     icon: academy,
+    iconBg: "#E6DEDD",
+    date: "Dec 2022 - Jan 2023",
+    points: [
+     " Assisted in teaching and mentoring students on Data Structures and Algorithm topics.",
+"Conducted code reviews,provide feedback,and helped students improve their coding skills."
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Food App",
+    description:
+      "Web-based platform that allows users to add items in their cart and then place their order and then store the details of customers and their order in firebase.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: food,
+    source_code_link: "https://github.com/sahil-6789/food-ordering-app",
+  },
+  {
+    name: "JobShala",
+    description:
+      "Web application that enables users to post blue collar jobs and other users can apply for that job by registering them  .",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Node",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Express",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "MongoDb",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: expense,
+    source_code_link: "https://github.com/sahil-6789/Jobshala-backend",
+  },
+  {
+    name: "Shopkart",
+    description:
+      "A fully fleged ecommerce MERN website with JWT authentication and payment gateway ",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "React",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Express",
+        color: "red-text-gradient",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Stripe",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Tailwind CSS",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: shopping,
+    source_code_link: "https://github.com/sahil-6789/shopkart",
+  },
+  {
+    name: "Tindog",
+    description:
+      "A Simple website for stray animals that can help them to get new homes ",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "green-text-gradient",
+      },
+    
+    ],
+    image: shopping,
+    source_code_link: "https://github.com/sahil-6789/tindog",
+  },
+  {
+    name: "Todo List",
+    description:
+      "Web application that enables users to create a list of their upcoming tasks and delete them when completetd  .",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "HTML",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: todo,
+    source_code_link: "https://github.com/sahil-6789/TodoList",
+  },
+  
+];
+
+export { services, technologies, experiences, testimonials, projects };
